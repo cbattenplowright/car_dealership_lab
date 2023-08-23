86f8a0fad058eec5779cbd766f7810c908028111
cbattenplowright <callumbp15@gmail.com>
2023-08-23 15:55:18 +0100

    create dealership module

---
+++ Dealership.js
@@ -0,0 +1,17 @@
+const Dealership = function(name, capacity, carStock){
+    this.name = name;
+    this.capacity = capacity;
+    this.carStock = carStock;
+}
+
+// GETTERS AND SETTERS
+
+Dealership.prototype.getName() = function() { return this.name };
+Dealership.prototype.getCapacity() = function() { return this.capacity };
+Dealership.prototype.getCarStock() = function() { return this.carStock };
+
+Dealership.prototype.setName() = function(name) { this.name };
+Dealership.prototype.setCapacity() = function(capacity) { this.capacity };
+Dealership.prototype.setCarStock() = function(carStock) { this.carStock };
+
+module.exports = Dealership;

91628c531dc3490694e90fef9ec8de88c181151d
cbattenplowright <callumbp15@gmail.com>
2023-08-23 16:10:53 +0100

    fixed syntax error with dealership getters and setters

--- Dealership.js
+++ Dealership.js
@@ -2,16 +2,16 @@ const Dealership = function(name, capacity, carStock){
     this.name = name;
     this.capacity = capacity;
     this.carStock = carStock;
-}
+};
 
 // GETTERS AND SETTERS
 
-Dealership.prototype.getName() = function() { return this.name };
-Dealership.prototype.getCapacity() = function() { return this.capacity };
-Dealership.prototype.getCarStock() = function() { return this.carStock };
+Dealership.prototype.getName = function() { return this.name };
+Dealership.prototype.getCapacity = function() { return this.capacity };
+Dealership.prototype.getCarStock = function() { return this.carStock };
 
-Dealership.prototype.setName() = function(name) { this.name = name };
-Dealership.prototype.setCapacity() = function(capacity) { this.capacity = capacity };
-Dealership.prototype.setCarStock() = function(carStock) { this.carStock = carStock };
+Dealership.prototype.setName = function(name) { this.name = name };
+Dealership.prototype.setCapacity = function(capacity) { this.capacity = capacity };
+Dealership.prototype.setCarStock = function(carStock) { this.carStock = carStock };
 
 module.exports = Dealership;

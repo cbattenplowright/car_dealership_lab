5a990fe126c04d6a466fa9ec9671dc306d3004e4
cbattenplowright <callumbp15@gmail.com>
2023-08-23 23:17:38 +0100

    fixed customer setters

--- Customer.js
+++ Customer.js
@@ -1,4 +1,4 @@
-const Customer = function(name, wallet) {
+const Customer = function (name, wallet) {
     this.name = name;
     this.wallet = wallet;
     this.car = null;
@@ -6,11 +6,11 @@ const Customer = function(name, wallet) {
 
 // GETTERS AND SETTERS
 
-Customer.prototype.getName = function() { return this.name; };
-Customer.prototype.getWallet = function() { return this.wallet; };
-Customer.prototype.getCar = function() { return this.car; };
+Customer.prototype.getName = function () { return this.name; };
+Customer.prototype.getWallet = function () { return this.wallet; };
+Customer.prototype.getCar = function () { return this.car; };
 
-Customer.prototype.setName = function(name) { this.name = name; };
-Customer.prototype.setWallet = function(wallet) { this.wallet = wallet; };
+Customer.prototype.setName = function (name) { this.name = name; };
+Customer.prototype.setWallet = function (wallet) { this.wallet = wallet; };
 
 module.exports = Customer;

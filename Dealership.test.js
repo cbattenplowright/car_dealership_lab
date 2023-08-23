0a6407daadf4f8de99ed6a5a3bb63c42ca4303a8
cbattenplowright <callumbp15@gmail.com>
2023-08-23 16:54:07 +0100

    added setter tests all pass

--- Dealership.test.js
+++ Dealership.test.js
@@ -1,9 +1,12 @@
 const Dealership = require('./Dealership');
+const Car = require("./Car");
 
 let dealership;
+let mercedes;
 
 beforeEach(() => {
-    dealership = new Dealership("Carlo's", 10, []);
+    mercedes = new Car("C63", "Mercedes", 180_000, "petrol");
+    dealership = new Dealership("Carlo's", 10, [mercedes]);
 });
 
 describe('getters', () => {
@@ -26,7 +29,7 @@ describe('getters', () => {
 
     test('can get car stock', () => {
         
-        expected = [];
+        expected = [mercedes];
         actual = dealership.getCarStock();
         expect(actual).toEqual(expected);
 
@@ -38,6 +41,33 @@ describe('setters', () => {
 
     test('can set name', () => {
 
-    })
+        dealership.setName("AutoTrader");
+        expected = "AutoTrader";
+        actual = dealership.getName();
+        expect(actual).toBe(expected);
+
+    });
+
+    test('can set capacity', () => {
+
+        dealership.setCapacity(50);
+        expected = 50;
+        actual = dealership.getCapacity();
+        expect(actual).toBe(expected);
+
+    });
+
+    test('can set Car stock', () => {
+
+        ford = new Car("GT40", "Ford", 150_000, "petrol");
+        ferrari = new Car('F40', 'Ferrari', 1_000_000, 'petrol');
+
+        dealership.setCarStock([ford, ferrari]);
+
+        expected = 2;
+        actual = dealership.getCarStock().length;
+        expect(actual).toBe(expected);
+
+    });
 
 });

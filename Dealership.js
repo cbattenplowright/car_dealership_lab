bee8bdc0d3588aaffb82d45ce384f3cd18d20ef4
cbattenplowright <callumbp15@gmail.com>
2023-08-23 17:05:03 +0100

    added getStockCount method to dealership

--- Dealership.js
+++ Dealership.js
@@ -1,17 +1,16 @@
-const Dealership = function(name, capacity, carStock){
+const Dealership = function(name, capacity){
     this.name = name;
     this.capacity = capacity;
-    this.carStock = carStock;
+    this.carStock = [];
 };
 
 // GETTERS AND SETTERS
 
 Dealership.prototype.getName = function() { return this.name };
 Dealership.prototype.getCapacity = function() { return this.capacity };
-Dealership.prototype.getCarStock = function() { return this.carStock };
+Dealership.prototype.getStockCount = function() { return this.carStock.length };
 
 Dealership.prototype.setName = function(name) { this.name = name };
 Dealership.prototype.setCapacity = function(capacity) { this.capacity = capacity };
-Dealership.prototype.setCarStock = function(carStock) { this.carStock = carStock };
 
 module.exports = Dealership;
--- Dealership.test.js
+++ Dealership.test.js
@@ -6,8 +6,9 @@ let mercedes;
 
 beforeEach(() => {
     mercedes = new Car("C63", "Mercedes", 180_000, "petrol");
-    dealership = new Dealership("Carlo's", 10, [mercedes]);
+    dealership = new Dealership("Carlo's", 10);
 });
+
 
 describe('getters', () => {
 
@@ -27,11 +28,11 @@ describe('getters', () => {
 
     });
 
-    test('can get car stock', () => {
+    test('can get stock count', () => {
         
-        expected = [mercedes];
-        actual = dealership.getCarStock();
-        expect(actual).toEqual(expected);
+        expected = 0;
+        actual = dealership.getStockCount();
+        expect(actual).toBe(expected);
 
     });
 
@@ -53,19 +54,6 @@ describe('setters', () => {
         dealership.setCapacity(50);
         expected = 50;
         actual = dealership.getCapacity();
-        expect(actual).toBe(expected);
-
-    });
-
-    test('can set Car stock', () => {
-
-        ford = new Car("GT40", "Ford", 150_000, "petrol");
-        ferrari = new Car('F40', 'Ferrari', 1_000_000, 'petrol');
-
-        dealership.setCarStock([ford, ferrari]);
-
-        expected = 2;
-        actual = dealership.getCarStock().length;
         expect(actual).toBe(expected);
 
     });

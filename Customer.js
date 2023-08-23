4f76d419b05bef2e9c19fe0b2f0e8daeb09f3cef
cbattenplowright <callumbp15@gmail.com>
2023-08-23 23:27:38 +0100

    added buyCar method and setCar method to Customer module

--- Customer.js
+++ Customer.js
@@ -4,6 +4,19 @@ const Customer = function (name, wallet) {
     this.car = null;
 };
 
+Customer.prototype.buyCar = function(car) { 
+
+    if (wallet >= car.getPrice) {
+        Customer.setCar(car);
+        wallet -= car.getPrice;
+        return `You have just purchased the ${this.car.getManufacturer} ${this.car.getModel} and have ${this.wallet} remaining in you wallet`;
+    } else {
+        return "You don't have enough money in your wallet to purchase the car";
+    }
+
+};
+
+
 // GETTERS AND SETTERS
 
 Customer.prototype.getName = function () { return this.name; };
@@ -12,5 +25,6 @@ Customer.prototype.getCar = function () { return this.car; };
 
 Customer.prototype.setName = function (name) { this.name = name; };
 Customer.prototype.setWallet = function (wallet) { this.wallet = wallet; };
+Customer.prototyp.setCar = function(car) { this.car = car };
 
 module.exports = Customer;

c5b03b42a547d47a7be56b17e10912fb22942fa5
cbattenplowright <callumbp15@gmail.com>
2023-08-23 20:42:15 +0100

    added getValueOfDealership method and tests

--- Dealership.js
+++ Dealership.js
@@ -1,10 +1,10 @@
-const Dealership = function(name, capacity){
+const Dealership = function (name, capacity) {
     this.name = name;
     this.capacity = capacity;
     this.carStock = [];
 };
 
-Dealership.prototype.addCar = function(car) { 
+Dealership.prototype.addCar = function (car) {
 
     if (this.getStockCount() === this.capacity) {
         return "Unavailable space in dealership to add car to stock";
@@ -14,7 +14,7 @@ Dealership.prototype.addCar = function(car) {
 
 };
 
-Dealership.prototype.filterByManufacturer = function(manufacturer) {
+Dealership.prototype.filterByManufacturer = function (manufacturer) {
     const carsFilteredByManufacturer = this.carStock.filter((car) => {
         return car.getManufacturer() === manufacturer;
     });
@@ -22,19 +22,25 @@ Dealership.prototype.filterByManufacturer = function(manufacturer) {
     return carsFilteredByManufacturer;
 };
 
-Dealership.prototype.getCarManufacturers = function() {
-    
-    const carManufacturers =  this.carStock.map((car) => { return car.manufacturer });
+Dealership.prototype.getCarManufacturers = function () {
+
+    const carManufacturers = this.carStock.map((car) => { return car.manufacturer });
     return carManufacturers;
+}
+
+Dealership.prototype.getValueOfDealership = function () {
+
+    const totalValue = this.carStock.reduce((accumulator, car) => accumulator + car.getPrice(), 0);
+    return totalValue;
 }
 
 // GETTERS AND SETTERS
 
-Dealership.prototype.getName = function() { return this.name };
-Dealership.prototype.getCapacity = function() { return this.capacity };
-Dealership.prototype.getStockCount = function() { return this.carStock.length };
+Dealership.prototype.getName = function () { return this.name };
+Dealership.prototype.getCapacity = function () { return this.capacity };
+Dealership.prototype.getStockCount = function () { return this.carStock.length };
 
-Dealership.prototype.setName = function(name) { this.name = name };
-Dealership.prototype.setCapacity = function(capacity) { this.capacity = capacity };
+Dealership.prototype.setName = function (name) { this.name = name };
+Dealership.prototype.setCapacity = function (capacity) { this.capacity = capacity };
 
 module.exports = Dealership;
--- Dealership.test.js
+++ Dealership.test.js
@@ -37,22 +37,14 @@ test('can\'t add car to stock', () => {
 
 });
 
-test('can get car manufacturers', () => {
-
-    expected = ["Ferrari", "Ford", "Mercedes"];
-    actual = dealership.getCarManufacturers();
-    expect(actual).toMatchObject(expected);
-
-});
-
 test('can find all cars from a given manufacturer', () => {
-
+    
     ferrari_F50 = new Car("F50", "Ferrari", 100_000, "diesel");
     dealership.addCar(ferrari_F50);
     expected = [ferrari_F40, ferrari_F50];
     actual = dealership.filterByManufacturer("Ferrari");
     expect(actual).toMatchObject(expected);
-
+    
 });
 
 describe('getters', () => {
@@ -81,6 +73,21 @@ describe('getters', () => {
 
     });
 
+    test('can get car manufacturers', () => {
+    
+        expected = ["Ferrari", "Ford", "Mercedes"];
+        actual = dealership.getCarManufacturers();
+        expect(actual).toMatchObject(expected);
+    
+    });
+
+    test('can get total value of dealership', () => {
+
+        expected = 1_280_000;
+        actual = dealership.getValueOfDealership();
+        expect(actual).toBe(expected);
+    
+    });
 });
 
 describe('setters', () => {

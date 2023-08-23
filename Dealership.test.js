a3d5f544b996a57859b091f95f6af63308fa2673
cbattenplowright <callumbp15@gmail.com>
2023-08-23 16:12:20 +0100

    written dealership tests

---
+++ Dealership.test.js
@@ -0,0 +1,35 @@
+const Dealership = require('./Dealership');
+
+let dealership;
+
+beforeEach(() => {
+    dealership = new Dealership("Carlo's", 10, []);
+});
+
+describe('getters', () => {
+
+    test('can get name', () => {
+
+        expected = "Carlo's";
+        actual = dealership.getName();
+        expect(actual).toBe(expected);
+
+    });
+
+    test('can get capacity', () => {
+
+        expected = 10;
+        actual = dealership.getCapacity();
+        expect(actual).toBe(expected);
+
+    });
+
+    test('can get car stock', () => {
+
+        expected = [];
+        actual = dealership.getCarStock();
+        expect(actual).toBe(expected);
+
+    });
+
+});
